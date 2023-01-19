378f9d3ae3330b33487f78758b4677cc3d920aa6
KaranRansing2002 <ransingkaran495@gmail.com>
2023-01-20 02:45:39 +0530

    10th commit

--- App.js
+++ App.js
@@ -2,21 +2,65 @@ const express = require("express");
 const { default: mongoose } = require("mongoose");
 const app = express();
 const cookieParser = require('cookie-parser');
-
 const cors = require("cors");
 const pokemonRouter = require("./routers/pokemonRouter");
 const movesRouter = require("./routers/movesRouter");
 const userRouter = require("./routers/userRouter");
+const http = require('http')
+const {Server} = require("socket.io")
+
+
 const corsOptions = {
   origin: "http://localhost:3000",
   credentials: true, //
 };
-app.use(cors(corsOptions)); // Use this after the variable declaration
 
+app.use(cors(corsOptions)); // Use this after the variable declaration
 app.use(express.json());
 app.use(cookieParser());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods : ['GET','POST']
+  }
+})
+
+io.on("connection", (socket) => {
+  console.log("user connected", socket.id)
  
-app.listen(8000, () => {
+  socket.on("join_room", (data) => {
+    socket.join(data);
+  })
+  
+  socket.on("send_message", (data) => {
+    console.log(data);
+    socket.to(data.roomid).emit("receive_message",data)
+  })
+
+  socket.on("pokemon", (data) => {
+    console.log(data);
+    socket.to(data.roomid).emit("opponent_pokemon",data)
+  })
+
+  socket.on("currhp", (data) => {
+    socket.to(data.roomid).emit("opponent_hp", data);
+  })
+
+  socket.on("attack", (data) => {
+    console.log(data);
+    socket.to(data.roomid).emit("opponent_attack", data);
+  })
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  })
+
+})
+
+server.listen(8000, () => {
   console.log("Server started.");  
 }); 
 
