ff80f77197e290c5b672442b58d5170113a2949b
KaranRansing2002 <ransingkaran495@gmail.com>
2023-02-04 13:34:29 +0530

    12th commit

--- App.js
+++ App.js
@@ -20,7 +20,7 @@ const corsOptions = {
 app.use(express.json());
 app.use(cookieParser());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Credentials", "true");
